4306e91d82aa3e28251ada235cd3fe2b2aaa656a
szahlner <stefan.zahlner@gmx.at>
2022-08-17 20:29:26 +0200

    update inverted_pendulum run scripts

--- scripts/run_sac_inverted_pendulum.sh
+++ scripts/run_sac_inverted_pendulum.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+env_name=InvertedPendulum-v2
+target_entropy=-0.05
+num_steps=16000
+start_steps=500
+updates_per_step=10
+
 # InvertedPendulum-v2 UTD 1
 for seed in 123 456 789
 do
-  python main_sac.py --env-name InvertedPendulum-v2 --target-entropy -0.05 --num-steps 16000 --start-steps 500 --updates-per-step 1 --seed $seed --eval-timesteps 250
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --updates-per-step 1 --seed $seed --eval-timesteps 250
 done
 
 # InvertedPendulum-v2 UTD 10
 for seed in 123 456 789
 do
-  python main_sac.py --env-name InvertedPendulum-v2 --target-entropy -0.05 --num-steps 16000 --start-steps 500 --updates-per-step 10 --seed $seed --eval-timesteps 250
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --updates-per-step $updates_per_step --seed $seed --eval-timesteps 250
 done
 
 # InvertedPendulum-v2 MBPO
 for seed in 123 456 789
 do
-  python main_sac.py --env-name InvertedPendulum-v2 --target-entropy -0.05 --num-steps 16000 --n-training-samples 100000 --start-steps 500 --model-based --updates-per-step 10 --v-ratio 0.95 --seed $seed --eval-timesteps 250
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --n-training-samples 100000 --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --seed $seed --eval-timesteps 250
 done
