bb4d2f03a8d4a73ceadb23a08994bba717a4c2af
szahlner <stefan.zahlner@gmx.at>
2022-08-18 09:24:27 +0200

    run scripts add halfcheetah

---
+++ scripts/run_ddpg_halfcheetah.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+env_name=HalfCheetah-v2
+num_steps=400000
+start_steps=5000
+updates_per_step=40
+rollout_min_epoch=20
+rollout_max_epoch=150
+rollout_min_length=1
+rollout_max_length=1
+
+# InvertedPendulum-v2 UTD 1
+for seed in 123 456 789
+do
+  python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --updates-per-step 1 --seed $seed
+done
+
+# InvertedPendulum-v2 UTD 10
+for seed in 123 456 789
+do
+  python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --updates-per-step $updates_per_step --seed $seed
+done
+
+# InvertedPendulum-v2 MBPO
+for seed in 123 456 789
+do
+  python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
+done
