a8d9ea789efa5d7e91cf79db9ac565e141d93384
szahlner <stefan.zahlner@gmx.at>
2022-08-18 09:18:25 +0200

    run scripts inverted pendulum add rollout parameters

--- scripts/run_ddpg_inverted_pendulum.sh
+++ scripts/run_ddpg_inverted_pendulum.sh
@@ -4,6 +4,10 @@ env_name=InvertedPendulum-v2
 num_steps=16000
 start_steps=500
 updates_per_step=10
+rollout_min_epoch=1
+rollout_max_epoch=15
+rollout_min_length=1
+rollout_max_length=1
 
 # InvertedPendulum-v2 UTD 1
 for seed in 123 456 789
@@ -20,5 +24,5 @@ done
 # InvertedPendulum-v2 MBPO
 for seed in 123 456 789
 do
-  python main_ddpg.py --env-name $env_name --num-steps $num_steps --n-training-samples 100000 --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --seed $seed --eval-timesteps 250
+  python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed --eval-timesteps 250
 done
--- scripts/run_sac_inverted_pendulum.sh
+++ scripts/run_sac_inverted_pendulum.sh
@@ -5,6 +5,10 @@ target_entropy=-0.05
 num_steps=16000
 start_steps=500
 updates_per_step=10
+rollout_min_epoch=1
+rollout_max_epoch=15
+rollout_min_length=1
+rollout_max_length=1
 
 # InvertedPendulum-v2 UTD 1
 for seed in 123 456 789
@@ -21,5 +25,5 @@ done
 # InvertedPendulum-v2 MBPO
 for seed in 123 456 789
 do
-  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --n-training-samples 100000 --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --seed $seed --eval-timesteps 250
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed --eval-timesteps 250
 done
