54e8c7c3f865be4d2d742e1006940e54c1f3d391
szahlner <stefan.zahlner@gmx.at>
2022-08-18 09:18:37 +0200

    run scripts hopper add rollout parameters

--- scripts/run_ddpg_hopper.sh
+++ scripts/run_ddpg_hopper.sh
@@ -4,6 +4,10 @@ env_name=Hopper-v2
 num_steps=125000
 start_steps=5000
 updates_per_step=20
+rollout_min_epoch=20
+rollout_max_epoch=150
+rollout_min_length=1
+rollout_max_length=15
 
 # InvertedPendulum-v2 UTD 1
 for seed in 123 456 789
@@ -20,5 +24,5 @@ done
 # InvertedPendulum-v2 MBPO
 for seed in 123 456 789
 do
-  python main_ddpg.py --env-name $env_name --num-steps $num_steps --n-training-samples 100000 --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --seed $seed
+  python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
 done
--- scripts/run_sac_hopper.sh
+++ scripts/run_sac_hopper.sh
@@ -5,6 +5,10 @@ target_entropy=-1
 num_steps=125000
 start_steps=5000
 updates_per_step=20
+rollout_min_epoch=20
+rollout_max_epoch=150
+rollout_min_length=1
+rollout_max_length=15
 
 # InvertedPendulum-v2 UTD 1
 for seed in 123 456 789
@@ -21,5 +25,5 @@ done
 # InvertedPendulum-v2 MBPO
 for seed in 123 456 789
 do
-  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --n-training-samples 100000 --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --seed $seed
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
 done
