994d1798330d881e014deb97bbdfa7da38c81e4b
szahlner <stefan.zahlner@gmx.at>
2022-08-22 14:49:25 +0200

    re-comment script files

--- scripts/run_ddpg_walker2d.sh
+++ scripts/run_ddpg_walker2d.sh
@@ -9,19 +9,19 @@ rollout_max_epoch=150
 rollout_min_length=1
 rollout_max_length=1
 
-# InvertedPendulum-v2 UTD 1
+# Walker2d-v2 UTD 1
 for seed in 123 456 789
 do
   python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --updates-per-step 1 --seed $seed
 done
 
-# InvertedPendulum-v2 UTD 10
+# Walker2d-v2 UTD 20
 for seed in 123 456 789
 do
   python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --updates-per-step $updates_per_step --seed $seed
 done
 
-# InvertedPendulum-v2 MBPO
+# Walker2d-v2 MBPO
 for seed in 123 456 789
 do
   python main_ddpg.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
--- scripts/run_sac_ant.sh
+++ scripts/run_sac_ant.sh
@@ -10,19 +10,19 @@ rollout_max_epoch=100
 rollout_min_length=1
 rollout_max_length=25
 
-# InvertedPendulum-v2 UTD 1
+# Ant-v2 UTD 1
 for seed in 123 456 789
 do
   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --updates-per-step 1 --seed $seed
 done
 
-# InvertedPendulum-v2 UTD 10
+# Ant-v2 UTD 20
 for seed in 123 456 789
 do
   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --updates-per-step $updates_per_step --seed $seed
 done
 
-# InvertedPendulum-v2 MBPO
+# Ant-v2 MBPO
 for seed in 123 456 789
 do
   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
