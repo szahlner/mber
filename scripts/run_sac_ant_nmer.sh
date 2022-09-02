8f53eba321ed3d737258f474bafa4af07bb75178
szahlner <stefan.zahlner@gmx.at>
2022-09-02 11:23:23 +0200

    add scripts nmer ant cheetah and walker2d

---
+++ scripts/run_sac_ant_nmer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+env_name=Ant-v2
+target_entropy=-4
+num_steps=300000
+start_steps=5000
+updates_per_step=20
+rollout_min_epoch=20
+rollout_max_epoch=100
+rollout_min_length=1
+rollout_max_length=25
+
+# Ant-v2 NMER
+for seed in 123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --nmer --updates-per-step $updates_per_step --seed $seed
+done
+
+# Ant-v2 NMER MBPO
+for seed in 123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --nmer --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
+done
---
+++ scripts/run_sac_halfcheetah_nmer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+env_name=HalfCheetah-v2
+target_entropy=-3
+num_steps=400000
+start_steps=5000
+updates_per_step=40
+rollout_min_epoch=20
+rollout_max_epoch=150
+rollout_min_length=1
+rollout_max_length=1
+
+# HalfCheetah-v2 NMER
+for seed in 123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --nmer --updates-per-step $updates_per_step --seed $seed
+done
+
+# HalfCheetah-v2 NMER MBPO
+for seed in 123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --nmer --model-based --updates-per-step $updates_per_step --v-ratio 0.95 --rollout-min-epoch $rollout_min_epoch --rollout-max-epoch $rollout_max_epoch --rollout-min-length $rollout_min_length --rollout-max-length $rollout_max_length --seed $seed
+done
