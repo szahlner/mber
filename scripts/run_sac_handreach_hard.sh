2d30a3b0b35132a35a19160670eb58e046823ea7
szahlner <stefan.zahlner@gmx.at>
2022-10-04 11:41:34 +0200

    nub1000 -> nub200

--- scripts/run_sac_handreach_hard.sh
+++ scripts/run_sac_handreach_hard.sh
@@ -4,17 +4,17 @@ env_name=ShadowHandReachHard-v1
 alpha=0.01
 num_steps=50000
 start_steps=500
-n_update_batches=1000
+n_update_batches=200
 # rollout_min_epoch=0
 # rollout_max_epoch=1
 # rollout_min_length=3
 # rollout_max_length=3
 
 # ShadowHandReach-v1 UTD 1
-for seed in 123 456 789
-do
-  python main_sac_her.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --alpha $alpha --seed $seed --eval-timesteps 500 --her-normalize
-done
+# for seed in 123 456 789
+# do
+#   python main_sac_her.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --alpha $alpha --seed $seed --eval-timesteps 500 --her-normalize
+# done
 
 # ShadowHandReach-v1 UTD 10
 for seed in 123 456 789
@@ -23,10 +23,10 @@ do
 done
 
 # ShadowHandReach-v1 HER UTD 1
-for seed in 123 456 789
-do
-  python main_sac_her.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --her --alpha $alpha --seed $seed --eval-timesteps 500 --her-normalize
-done
+# for seed in 123 456 789
+# do
+#   python main_sac_her.py --env-name $env_name --num-steps $num_steps --start-steps $start_steps --her --alpha $alpha --seed $seed --eval-timesteps 500 --her-normalize
+# done
 
 # ShadowHandReach-v1 HER UTD 10
 for seed in 123 456 789
