08a6bbba6a8a022adf68b7144f92c609dab36da9
szahlner <stefan.zahlner@gmx.at>
2022-10-07 09:34:39 +0200

    update run scripts

--- scripts/run_sac_walker2d_per.sh
+++ scripts/run_sac_walker2d_per.sh
@@ -7,13 +7,13 @@ start_steps=5000
 updates_per_step=20
 
 # Walker2d-v2 PER
-for seed in 123 456 789
-do
-  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --per --seed $seed
-done
+# for seed in 123 456 789
+# do
+#   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --per --seed $seed
+# done
 
 # Walker2d-v2 PER utd 20
-for seed in 123 456 789
+for seed in 789  # 123 456 789
 do
   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --per --updates-per-step $updates_per_step --seed $seed
 done
