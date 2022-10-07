5bb0fdab72540e0a0302f0bb7b88465809149b91
szahlner <stefan.zahlner@gmx.at>
2022-10-07 09:28:38 +0200

    update run scripts

--- scripts/run_sac_halfcheetah_per.sh
+++ scripts/run_sac_halfcheetah_per.sh
@@ -7,10 +7,10 @@ start_steps=5000
 updates_per_step=40
 
 # HalfCheetah-v2 PER
-for seed in 123 456 789
-do
-  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --per --seed $seed
-done
+# for seed in 123 456 789
+# do
+#   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --per --seed $seed
+# done
 
 # HalfCheetah-v2 PER utd 40
 for seed in 123 456 789
