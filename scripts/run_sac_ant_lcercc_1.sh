4dd6f3eb013ecf272aeac2f2cb9331d7422d91de
szahlner <stefan.zahlner@gmx.at>
2022-10-14 19:10:58 +0200

    add ant torch lcercc run script

---
+++ scripts/run_sac_ant_lcercc_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env_name=Ant-v2
+target_entropy=-4
+num_steps=300000
+start_steps=5000
+updates_per_step=20
+
+# Ant-v2 LCERCC
+for seed in 456
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --lcercc --updates-per-step $updates_per_step --seed $seed
+done
