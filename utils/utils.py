5fe27489fe915b7dae6858fb4ae188c3dcd74920
szahlner <stefan.zahlner@gmx.at>
2022-10-14 12:58:59 +0200

    new minibatch kmeans

--- utils/utils.py
+++ utils/utils.py
@@ -206,12 +206,11 @@ class TensorMinibatchKMeans:
         self._cluster_centers_sum_sq = torch.zeros_like(self._cluster_centers, dtype=torch.float, device=self._device)
         self._cluster_centers_count = torch.zeros_like(self._cluster_centers, dtype=torch.float, device=self._device)
 
-        labels_numpy = self._labels.detach().cpu().numpy()
-        for n in range(len(labels_numpy)):
+        for n in range(len(self._labels)):
             ones = torch.ones(size=(self._dimensions,), dtype=torch.int, device=self._device)
-            self._cluster_centers_count[labels_numpy[n]] += ones
-            self._cluster_centers_sum[labels_numpy[n]] += X[n]
-            self._cluster_centers_sum_sq[labels_numpy[n]] += X[n] ** 2
+            self._cluster_centers_count[self._labels[n]] += ones
+            self._cluster_centers_sum[self._labels[n]] += X[n]
+            self._cluster_centers_sum_sq[self._labels[n]] += X[n] ** 2
 
         squared_mean = self._cluster_centers_sum_sq / self._cluster_centers_count
         max_or_eps = torch.max(self._eps, squared_mean - torch.square(self._cluster_centers))
@@ -228,12 +227,11 @@ class TensorMinibatchKMeans:
         _, idx = torch.min(dist, dim=-1)
 
         # Update cluster centers
-        idx_numpy = idx.detach().cpu().numpy()
-        for n in range(len(idx_numpy)):
+        for n in range(len(idx)):
             ones = torch.ones(size=(self._dimensions,), dtype=torch.int, device=self._device)
-            self._cluster_centers_count[idx_numpy[n]] += ones
-            self._cluster_centers_sum[idx_numpy[n]] += X[n]
-            self._cluster_centers_sum_sq[idx_numpy[n]] += X[n] ** 2
+            self._cluster_centers_count[idx[n]] += ones
+            self._cluster_centers_sum[idx[n]] += X[n]
+            self._cluster_centers_sum_sq[idx[n]] += X[n] ** 2
 
         # Update properties
         # self._labels = torch.cat((self._labels, idx), dim=-1)
