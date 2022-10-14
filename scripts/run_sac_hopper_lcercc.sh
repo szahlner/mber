e7f1c2eea00bd3ea6f9ef42bde5a6f370a6ce213
szahlner <stefan.zahlner@gmx.at>
2022-10-14 18:57:11 +0200

    update lcercc hopper run script

--- scripts/run_sac_hopper_lcercc.sh
+++ scripts/run_sac_hopper_lcercc.sh
@@ -7,7 +7,7 @@ start_steps=5000
 updates_per_step=20
 
 # Hopper-v2 LCERCC
-for seed in 789  # 123 456 789
+for seed in 123 456 789
 do
   python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --lcercc --updates-per-step $updates_per_step --seed $seed
 done
