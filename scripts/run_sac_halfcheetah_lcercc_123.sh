9e2403981c511fe313ca27bd3bf724a4f52057db
szahlner <stefan.zahlner@gmx.at>
2022-10-27 16:20:29 +0200

    add single lcercc scripts for ant and halfcheetah

---
+++ scripts/run_sac_halfcheetah_lcercc_123.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env_name=HalfCheetah-v2
+target_entropy=-3
+num_steps=400000
+start_steps=5000
+updates_per_step=40
+
+# HalfCheetah-v2 LCERCC
+for seed in 123  # 123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --lcercc --updates-per-step $updates_per_step --seed $seed
+done
