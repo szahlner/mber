f1d3bb356380fbe54a9b3fd1936de61064ce191e
szahlner <stefan.zahlner@gmx.at>
2022-11-21 14:52:44 +0100

    update run sac iiwa

--- scripts/run_sac_iiwa.sh
+++ scripts/run_sac_iiwa.sh
@@ -1,1 +1,9 @@
-https://gitlab.tuwien.ac.at/e376-acin/v4r/education/robot-vision/2022/kuka-robosuite.git
+#!/bin/bash
+# Select right directory
+cd "/home/"
+
+# Remove existing
+rm -r ./kuka-robosuite
+
+# Clone new
+git clone https://gitlab.tuwien.ac.at/e376-acin/v4r/education/robot-vision/2022/kuka-robosuite.git
