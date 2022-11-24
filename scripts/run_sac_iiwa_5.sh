80e956c8d482d1fdb8a7f2f23fff382f23c209d6
szahlner <stefan.zahlner@gmx.at>
2022-11-24 10:14:31 +0100

    update iiwa_extended run scripts

---
+++ scripts/run_sac_iiwa_5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env_name=IIWA14_extended
+target_entropy=-5
+num_steps=500000
+start_steps=5000
+# updates_per_step=20
+
+# IIWA14_extended UTD 1
+for seed in 123 #  123 456 789
+do
+  python main_sac.py --env-name $env_name --target-entropy $target_entropy --num-steps $num_steps --start-steps $start_steps --updates-per-step 1 --seed $seed
+done
