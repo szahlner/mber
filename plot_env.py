9fde0bd93df628cb729d9912cccb3572f17cda5f
szahlner <stefan.zahlner@gmx.at>
2022-11-30 21:05:59 +0100

    add plot_env

---
+++ plot_env.py
@@ -0,0 +1,32 @@
+import gym
+import shadowhand_gym
+import robosuite as suite
+from robosuite.wrappers import GymWrapper
+from robosuite.controllers import load_controller_config
+
+
+env_id = "Lift"
+env = GymWrapper(
+    suite.make(
+        env_id,
+        robots="IIWA14_extended",  # use Sawyer robot
+        use_camera_obs=False,  # do not use pixel observations
+        has_offscreen_renderer=False,  # not needed since not using pixel obs
+        has_renderer=True,  # make sure we can render to the screen
+        reward_shaping=True,  # use dense rewards
+        control_freq=20,  # control should happen fast enough so that simulation looks smooth
+        horizon=1000,
+        controller_configs=load_controller_config(default_controller="OSC_POSE"),
+        renderer="mujoco",
+    )
+)
+env._max_episode_steps = 1000
+
+obs = env.reset()
+done = False
+while not done:
+    # Random action
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+
+env.close()
